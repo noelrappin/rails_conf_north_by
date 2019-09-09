5db1de513301a791eff990963fc18a8eef561d83
Noel Rappin <noelrappin@gmail.com>
2019-09-09 10:55:50 -0500

    fix

--- app/workflows/creates_concert.rb
+++ app/workflows/creates_concert.rb
@@ -9,9 +9,14 @@ class CreatesConcert
   def run
     Concert.transaction do
       @concert = Concert.create!(params)
-      concert.venue.total_seats.times do
-        concert.tickets.create!(status: :unsold)
+      concert.venue.rows.times do |row_number|
+      concert.venue.seats_per_row.times do |seat_number|
+        concert.tickets.create!(
+            row: row_number + 1,
+            number: seat_number + 1,
+            status: "unsold")
       end
+    end
     end
   end
 
